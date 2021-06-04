508083a03ae21bd6486a36ea4e05c0816e372501
徐无敌 <1150095145@qq.com>
2021-06-04 10:04:54 +0800

    修改获取周一和周日的逻辑

--- app/src/main/java/com/example/jorunal_bishe/util/JDateKit.java
+++ app/src/main/java/com/example/jorunal_bishe/util/JDateKit.java
@@ -42,7 +42,17 @@ public class JDateKit {
      */
     public static Date getFirstDayOfWeek() {
         Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        // 判断当前日期是一个星期的第几天
+        int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+        if (1 == dayWeek) {
+            cal.add(Calendar.DAY_OF_MONTH, -1);
+        }
+        //设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        //根据日历的规则，给当前日期减去星期几与一个星期第一天的差值
+        cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
         return cal.getTime();
     }
 
@@ -53,8 +63,19 @@ public class JDateKit {
      */
     public static Date getLastDayOfWeek() {
         Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
-        cal.add(Calendar.WEEK_OF_YEAR, 1);
+        // 判断当前日期是一个星期的第几天
+        int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+        if (1 == dayWeek) {
+            cal.add(Calendar.DAY_OF_MONTH, -1);
+        }
+        //设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        //根据日历的规则，给当前日期减去星期几与一个星期第一天的差值  得到周一
+        cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
+        //得到周日
+        cal.add(Calendar.DATE, 6);
         return cal.getTime();
     }
 
